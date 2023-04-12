ed16a19da6bb7120396a75323080d2c671b97fec
viniciuscs001 <21867068-viniciuscs001@users.noreply.replit.com>
2023-04-12 22:42:01 +0000

    Projeto para certificado freecodecamp (1)

--- index.js
+++ index.js
@@ -24,6 +24,58 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// Testando o new Date()
+// app.get('/api/2015-12-25', (req, res) => {
+  
+//   // Pega a data desejada
+//   const date = new Date('2015-12-25')
+  
+//   // Colocar no formato unix
+//   const unixTimeStamp = date.getTime()
+  
+//   // Pega a string com UTC
+//   const utcDateString = date.toUTCString();
+
+//   // Renderizando
+//   res.json({unix: unixTimeStamp + ' ', utc: utcDateString})
+  
+// })
+
+app.get('/api/:date?', (req, res, next) => {
+  // Pegando o valor da URL
+  let date;
+  if (req.params.date) {
+    date = decodeURIComponent(req.params.date);
+    
+    // Verifica se na URL tem traço ou não
+    if(date.includes('-') || date.includes(' ')) {
+      date = new Date(date)
+    } else {
+      date = new Date(parseInt(date))
+    }
+  } else {
+    date = new Date();
+  }
+  
+  // Com essa data, transforma para unix e UTC
+  let unixTimeStamp = date.getTime()
+  let utcDataString = date.toUTCString()
+
+  // Cria um objeto JSON
+  let jsonObj = {
+    unix: unixTimeStamp,
+    utc: utcDataString
+  }
+
+  if(utcDataString == 'Invalid Date') {
+    res.json({ error: "Invalid Date" })
+  } else {
+    res.json(jsonObj)
+  }
+
+  // Renderiza o JSON
+  // res.json({ unix: unixTimeStamp, utc: utcDataString })
+})
 
 
 // listen for requests :)
